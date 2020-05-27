e8ab46ce03a2d5ef0fa8e6bb1957e6425d7f0117
Hugo Zhu <hugozhu@gmail.com>
2020-05-27 22:03:04 +0800

    added image upload

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module push
 
 go 1.13
 
-require github.com/hugozhu/godingtalk v1.0.2 // indirect
+require github.com/hugozhu/godingtalk v1.0.2
--- push.go
+++ push.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"os"
-    "regexp"
+	"regexp"
 
 	dingtalk "github.com/hugozhu/godingtalk"
 )
 
+var img *bool
+var f *string
+
+func init() {
+	img = flag.Bool("img", false, "Sending Image")
+	f = flag.String("f", "", "File Path")
+	flag.Parse()
+}
+
 func stripeMarkdown(str string) string {
-   	str = regexp.MustCompile("[*|#]+").ReplaceAllString(str, "")
+	str = regexp.MustCompile("[*|#]+").ReplaceAllString(str, "")
 	str = regexp.MustCompile("\\s+").ReplaceAllString(str, " ")
 	str = regexp.MustCompile("^ ").ReplaceAllString(str, "")
 	return str
@@ -17,5 +28,20 @@ func stripeMarkdown(str string) string {
 func main() {
 	c := dingtalk.NewDingTalkClient(os.Getenv("corpid"), os.Getenv("corpsecret"))
 	c.RefreshAccessToken()
-	c.SendRobotMarkdownMessage(os.Getenv("token"), stripeMarkdown(os.Args[1]), os.Args[1])
+	if *img {
+		f, err := os.Open(*f)
+		defer f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		m, err := c.UploadMedia("image", "screenshot.jpg", f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%v %v %v", m.MediaID, *f, err)
+		markdown := "![Screenshot](" + m.MediaID + ")"
+		c.SendRobotMarkdownMessage(os.Getenv("token"), "Screenshot", markdown)
+	} else {
+		c.SendRobotMarkdownMessage(os.Getenv("token"), stripeMarkdown(os.Args[1]), os.Args[1])
+	}
 }
