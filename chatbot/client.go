04059d70ec58195d1e5eb026e33c055b685e5f7e
一粟 <hugozhu@alibaba-inc.com>
2021-08-02 15:47:35 +0800

    added chatbot client

---
+++ chatbot/client.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	proto "github.com/hugozhu/ding_robot_push/chatbot/chat" // 根据proto文件自动生成的代码
+	"google.golang.org/grpc"
+)
+
+var workDirPath string
+
+func init() {
+	var err error
+	workDirPath, err = os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func main() {
+	conn, err := grpc.Dial("localhost:3000", grpc.WithInsecure())
+	if err != nil {
+		log.Printf("Failed to connect: [%v]\n", err)
+		return
+	}
+	defer conn.Close()
+
+	// 声明客户端
+	client := proto.NewChatClient(conn)
+
+	// 声明 context
+	ctx := context.Background()
+
+	// 创建双向数据流
+	stream, err := client.BidStream(ctx)
+	if err != nil {
+		log.Printf("Streaming error: [%v]\n", err)
+	}
+
+	go func() {
+		for {
+			if err := stream.Send(&proto.Request{Input: "/ping"}); err != nil {
+				log.Println(err)
+			}
+			time.Sleep(time.Duration(5) * time.Second)
+		}
+	}()
+
+	for {
+		message, err := stream.Recv()
+		if err != nil {
+			log.Println("Error:", err)
+			break
+		}
+		log.Printf("[Received]: %s", message.Output)
+		text := message.Output
+		if strings.HasPrefix(text, "/pong") {
+
+		} else {
+			cmd := exec.Command("/bin/bash", workDirPath+"/cmd.sh", text)
+			var stderr, stdout bytes.Buffer
+			cmd.Stdout = &stdout
+			cmd.Stderr = &stderr
+			err = cmd.Run()
+			if err != nil {
+				log.Println(err.Error(), stderr.String())
+			} else {
+				log.Println(stdout.String())
+			}
+		}
+	}
+}
--- chatbot/server.go
+++ chatbot/server.go
@@ -3,21 +3,19 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
-	"fmt"
-	"net"		
-	"strconv"
 
 	dingtalk "github.com/hugozhu/godingtalk"
 
-	proto "github.com/hugozhu/ding_robot_push/chatbot/chat" // 自动生成的 proto代码	
+	proto "github.com/hugozhu/ding_robot_push/chatbot/chat" // 自动生成的 proto代码
 	"google.golang.org/grpc"
-
 )
 
 var workDirPath string
@@ -47,45 +45,29 @@ func (s *Streamer) BidStream(stream proto.Chat_BidStreamServer) error {
 		case <-ctx.Done():
 			sid := fmt.Sprintf("%v", stream)
 			delete(clients, sid)
-			log.Println("收到客户端通过context发出的终止信号")
+			// log.Println("收到客户端通过context发出的终止信号")
 			return ctx.Err()
 		default:
 			// 接收从客户端发来的消息
 			message, err := stream.Recv()
 			sid := fmt.Sprintf("%v", &stream)
-			log.Println("message from " + sid)
 			if err == io.EOF {
-				log.Println("客户端发送的数据流结束")
+				// log.Println("客户端发送的数据流结束")
 				return nil
 			}
 			if err != nil {
-				log.Println("接收数据出错:", err)
+				// log.Println("接收数据出错:", err)
 				return err
 			}
 			clients[sid] = stream
 			// 如果接收正常，则根据接收到的 字符串 执行相应的指令
 			switch message.Input {
-			case "结束对话\n":
-				log.Println("收到'结束对话'指令")
-				if err := stream.Send(&proto.Response{Output: "收到结束指令"}); err != nil {
+			case "/ping":
+				if err := stream.Send(&proto.Response{Output: "/pong"}); err != nil {
 					return err
 				}
-				// 收到结束指令时，通过 return nil 终止双向数据流
-				return nil
-
-			case "返回数据流\n":
-				log.Println("收到'返回数据流'指令")
-				// 收到 收到'返回数据流'指令， 连续返回 10 条数据
-				for i := 0; i < 10; i++ {
-					if err := stream.Send(&proto.Response{Output: "数据流 #" + strconv.Itoa(i)}); err != nil {
-						return err
-					}
-				}
-
 			default:
-				// 缺省情况下， 返回 '服务端返回: ' + 输入信息
 				log.Printf("[收到消息]: %s", message.Input)
-				broadCast(message.Input)
 			}
 		}
 	}
@@ -106,6 +88,14 @@ func handler(w http.ResponseWriter, req *http.Request) {
 		log.Println("text: ", text)
 		if text != "" {
 			exec.Command("/bin/bash", workDirPath+"/push.sh", text).Run()
+		}
+	} else if action == "send" {
+		text := req.URL.Query().Get("text")
+		err := broadCast(text)
+		if err != nil {
+			io.WriteString(w, "{ \"errcode\": 1, \"errmsg\": \", err.Error() ,\"}")
+		} else {
+			io.WriteString(w, "{ \"errcode\": 0, \"errmsg\": \"ok\"}")
 		}
 	} else {
 		log.Printf("%v", req)
@@ -151,13 +141,13 @@ func main() {
 	clients = make(map[string]proto.Chat_BidStreamServer)
 
 	// 注册 ChatServer
-	proto.RegisterChatServer(server, &Streamer{})	
+	proto.RegisterChatServer(server, &Streamer{})
 	address, err := net.Listen("tcp", ":3000")
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("Chat Server listens on 3000")	
+	log.Printf("Chat Server listens on 3000")
 	if err := server.Serve(address); err != nil {
 		panic(err)
-	}	
+	}
 }
